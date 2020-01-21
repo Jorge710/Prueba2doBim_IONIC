08c28fae16f8982ff03758033f56427e4cc52ca5
Jorge Iza <izajorge7@gmail.com>
2020-01-21 12:44:37 -0500

    APP con splash screen

--- app-AgendaCitas/src/app/app.component.ts
+++ app-AgendaCitas/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+//import { timer } from 'rxjs/observable/timer';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -18,7 +20,20 @@ export class AppComponent {
     this.initializeApp();
   }
 
+  showSplash = true; // <-- show animation
+  
   initializeApp() {
+    this.platform.ready().then(() => {
+
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();  // <-- hide static image
+
+      setTimeout(() => {
+        this.showSplash = false;
+      }, 3000);
+      //timer(3000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
+    });
+
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
